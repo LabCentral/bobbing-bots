f0c577621f0699df8792e8732a7cb1cc580b3c2a
Jonathan Hogg <me@jonathanhogg.com>
2019-02-04 15:44:41 +0000

    Basic template

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
-basic.showLeds(`
-    . # . # .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `)
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 11, NeoPixelMode.RGB)
+input.setAccelerometerRange(AcceleratorRange.TwoG)
 basic.forever(function () {
-	
+
 })
