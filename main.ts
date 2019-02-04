1b9e7916c0a9876491fc43c800539b1879e3789a
Jonathan Hogg <me@jonathanhogg.com>
2019-02-04 16:25:57 +0000

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 11, NeoPixelMode.RGB)
+strip.setBrightness(127)
 input.setAccelerometerRange(AcceleratorRange.TwoG)
 basic.forever(function () {
-
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
 })
