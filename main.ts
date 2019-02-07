93f62e513f58000034960c189534d5048067de95
Jonathan Hogg <me@jonathanhogg.com>
2019-02-07 14:02:25 +0000

    Simpler starting program

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 strip.setBrightness(127)
-input.setAccelerometerRange(AcceleratorRange.TwoG)
 basic.forever(function () {
-    strip.showBarGraph(input.acceleration(Dimension.Strength), 2000)
+    strip.showBarGraph(1, 6)
 })
